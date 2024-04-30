99baf92bb25b73575bbb0d7c9013c07b2cc17b31
cluelessmonk <neverstopwithme@gmail.com>
2024-04-30 15:06:47 +0530

    deployment script updated

--- src/pages/CallforPapers.jsx
+++ src/pages/CallforPapers.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import styles from "../styles/CallForPapers.module.css";
+import { Link, useNavigate } from "react-router-dom";
 
 const CallforPapers = () => {
+  const navigate = useNavigate();
   return (
     <div>
       <br />
@@ -14,21 +16,33 @@ const CallforPapers = () => {
             Topics of interest include but are not limited to:
           </p>
           <ul className={styles["bullet-points"]}>
-            <li>Nanotechnology in diagnostic applications</li>
-            <li>Omics technology as new dimension in disease diagnosis</li>
+            <li className={styles.important}>
+              Nanotechnology in diagnostic applications
+            </li>
+            <li className={styles.important}>
+              Omics technology as new dimension in disease diagnosis
+            </li>
             <li className={styles.important}>
               Advancements in cancer diagnostics
             </li>
-            <li>Development of liquid biopsy based assays</li>
-            <li>Point of care and affordable diagnostics</li>
-            <li>Miniaturization and automation of diagnostic methods</li>
+            <li className={styles.important}>
+              Development of liquid biopsy based assays
+            </li>
+            <li className={styles.important}>
+              Point of care and affordable diagnostics
+            </li>
+            <li className={styles.important}>
+              Miniaturization and automation of diagnostic methods
+            </li>
             <li className={styles.important}>
               Precision medicine and digital health
             </li>
             <li className={styles.important}>
               Impact and use of AI and ML in diagnostics
             </li>
-            <li>Advanced imaging techniques for diagnostics</li>
+            <li className={styles.important}>
+              Advanced imaging techniques for diagnostics
+            </li>
           </ul>
           <p>
             Submissions must be in PDF format and should not exceed 8 pages. All
@@ -56,7 +70,14 @@ const CallforPapers = () => {
               </tr>
             </tbody>
           </table>
-          <button className="btn btn-primary mt-3">Submit Paper</button>
+          <button
+            className="btn btn-primary mt-3 bg-blue-200 w-40 h-8 rounded-lg hover:scale-110"
+            onClick={() => {
+              navigate("../submit_your_paper");
+            }}
+          >
+            Submit Paper
+          </button>
         </div>
       </div>
     </div>
